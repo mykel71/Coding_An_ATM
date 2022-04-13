98a578f2bfc7b685c866af2d8d4cb5b0cab87ae3
Shepherd <shepherd.munemo@outlook.com>
2022-04-13 13:30:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class cardHolder
+{
+    string cardNum;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    public string getNum()
+    {
+        return cardNum;
+    }
+
+}
